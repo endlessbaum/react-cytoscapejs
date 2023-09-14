8237c439a318efae4ae24f4731054c6bc5f125bc
endlessbaum <shota6396@gmail.com>
2023-09-14 20:34:06 +0900

    fix import react

--- src/component.js
+++ src/component.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 // import { types } from './types';
 import { defaults } from './defaults';
 import Cytoscape from 'cytoscape';
